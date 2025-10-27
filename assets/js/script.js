edaa079c50841ce36c175700f194d54441db0bf7
marcoszori <marcosviniciusmarcos241@gmail.com>
2025-10-27 11:15:45 -0300

    atividade
    Parei na 5.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,11 +41,70 @@ if (userAge >=18) {
 //ATIVIDADE 3 - Condições
 //3.1
 
-let userAge = prompt("Digite um número?");
-userAge = Number(userAge);
-if (userAge /= 0) {
-    console.log("O número é par.")
+let num = prompt("Digite um número: ");
+if (num%2 == 0){
+    console.log("O número é par.");
 } else{
-    console.log("O número é ímpar.")
+    console.log("O número é ímpar.");
+}
+//3.2
+
+let ida = prompt("Digite sua idade:")
+
+if (ida>= 18){
+    console.log("Você pode dirigir:)")
+} else{
+    console.log("Você não pode dirigir:(")
+}
+
+//3.3
+
+let nota = prompt("Digite sua nota (0 a 10):");
+if ( nota < 5) {
+    console.log("Você está de REPROVADO:(");
+} else if (nota >= 5 && nota < 7) {
+    console.log("Você está de RECUPERAÇÃO!!");
+} 
+else if (nota >= 7 && nota <= 10) {
+    console.log("Você está APROVADO:)");
+}
+
+//ATIVIDADE 4 - Laços de Repetição
+
+//4.1
+
+for (let x = 1; x<=10; x++){
+    console.log(x)
+}
+
+//4.2
+
+let val = prompt("Digite um número para a tabuada:");
+
+for (let i = 1; i<=10; i++){
+    console.log(`${val} x ${i} = ${val*i}`);
+}
+
+//4.3
+
+let senha = prompt("Digite sua senha:");
+while (senha !== "1234") {
+    alert ("Senha Incorreta:(");
+    senha = prompt("Digite sua senha:");
+}
+alert("Acesso Permitido:)");
+
+//ATIVIDADE 5 - Funções
+
+//5.1
+
+let num1 = Number(prompt("Digite o Primeiro número:"));
+let num2 = Number(prompt("Digite o Segundo número:"));
+
+let resul (num1,num2){
+
+} 
+function soma(num1, num2) {
+    return num1 + num2;
 }
 
